5344944b6a90152d387d5a5e6cb97ab61e5bff92
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-16 18:19:41 +0530

    Updated the home theme

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { ReactComponent as LinkedInIcon } from '../images/linkedin.svg';
+import { ReactComponent as GitHubIcon } from '../images/github.svg';
+import { ReactComponent as CodechefIcon } from '../images/codechef.svg';
+import { ReactComponent as MailIcon } from '../images/mail.svg';
 import profilePicture from '../images/mukund.jpg';
 
 const Home = () => {
+  const email = 'mukundsolanki665@gmail.com';
+  const handleEmailClick = () => {
+    window.location.href = `mailto:${email}`;
+  };
   return (
     <div className="home-container">
       <div className="home-text">
@@ -9,14 +17,30 @@ const Home = () => {
         <h1 id='head2'>I am Mukund Solanki</h1>
         <h2 id='head3'>A software developer from India</h2>
         <p id='tagline'>"Transforming Ideas into Functional Reality"</p>
+
+        <div className="platform-icons">
+          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+            <LinkedInIcon className="icon" />
+          </a>
+          <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+            <GitHubIcon className="icon" />
+          </a>
+          <a href="https://www.codechef.com/" target="_blank" rel="noopener noreferrer">
+            <CodechefIcon className="icon" />
+          </a>
+          <a onClick={handleEmailClick} href="" target="_blank" rel="noopener noreferrer">
+            <MailIcon className="icon" />
+          </a>
+        </div>
       </div>
       <div className="profile-picture-container">
         <img
           src={profilePicture}
-          alt="Profile Picture"
+          alt="Profile"
           className="profile-picture"
         />
       </div>
+
     </div>
   );
 };
